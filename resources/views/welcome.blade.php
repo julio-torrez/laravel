56e142572d2b3ee4401f0e89578bac9832e1f6af
julio <julioces_torrez@outlook.com>
2022-08-08 11:38:10 -0400

    complete mi nombre completo en el titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,16 +81,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   JULIO CESAR
+                   JULIO CESAR TORREZ TORRICO
                 </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentos</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">Nuevos</a>
-                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://blog.laravel.com">MIBlog</a>
                     <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://forge.laravel.com">FUERZA</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
