36ed14bf3a54c27fb11b25f054c6d7f0967ab981
julio-torrez <julioces_torrez@outlook.com>
2022-08-13 11:03:23 -0400

    aumente ocupasion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   JULIO CESAR TORREZ TORRICO
+                   JULIO CESAR TORREZ TORRICO 
+                   ESTUDIANTE DE UMSS
                 </div>
 
                 <div class="links">
